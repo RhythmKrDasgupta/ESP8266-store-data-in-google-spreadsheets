7abd501eff6ad319e98cd3b6058359361fd5ece9
Rhythm Kr Dasgupta <rhythmdas0@gmail.com>
2018-07-27 15:59:41 +0530

    Add files via upload

---
+++ google_sheet.ino
@@ -0,0 +1,82 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include "DHT.h"
+#define DHTPIN D1
+#define DHTTYPE DHT11 
+ #include "SparkFun_UHF_RFID_Reader.h"
+const char* ssid     = "linksys";
+const char* password = "secretcube";
+const char* host = "script.google.com"; 
+const char* fingerprint = "ad a8 c4 32 4f f1 e4 7a 59 17 2d 4b a4 91 fd 5a 0e ce 2a d5";
+String url;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  dht.begin();
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password); 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+}
+
+void loop() {
+ 
+  Serial.print("connecting to ");
+  Serial.println(host);
+ 
+  WiFiClientSecure client;
+
+  const int httpPort = 443;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+ if (client.verify(fingerprint, host)) {
+  Serial.println("certificate matches");
+  } else {
+  Serial.println("certificate doesn't match");
+  }
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+     Serial.println("Failed to read from DHT sensor!");
+     return;
+  }
+  
+  url = "/macros/s/AKfycby0PEAEWsmzPJ_weMsYmWKOOVim8bLJ3qOwWwryT850tpF-9VQ/exec?func=addData&val="+ String(t);
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(500);
+  String section="header";
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  Serial.println();
+  Serial.println("closing connection");
+  delay(100);
+}
